75feabcd90395a2efc594677e920ab032b553903
nikusha95 <nikasanaia11@yahoo.com>
2023-03-25 13:07:38 +0400

    INIT

---
+++ Multidimension/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[,] multiple = new int[3,4]
+{
+  /*0*/  {1,2,3,4},
+  /*1*/   {6,7,8,9},
+  /*2*/  {11,12,13,14}
+};
+
+for (int i = 0; i < multiple.GetLength(0); i++)
+{
+    for (int j = 0; j < multiple.GetLength(1); j++)
+    {
+        Console.WriteLine(multiple[i,j]);
+    }
+}
+
+
+int [][] jaggedArray = new int [3][];
+/*
+ * 1,2,3
+ * 1,2,3,4
+ * 1,2,3,4,5,7,8
+ */
+for (int i = 0; i < jaggedArray.GetLength(0); i++)
+{
+    Console.WriteLine("enter dimension");
+    int p = int.Parse(Console.ReadLine());
+
+    jaggedArray[i] = new int[p];
+
+    for (int j = 0; j < jaggedArray[i].Length; j++)
+    {
+       int z =  int.Parse(Console.ReadLine());
+       jaggedArray[i][j] = z;
+    }
+}
+
+foreach (int [] subArray in jaggedArray)
+{
+    foreach (var element in subArray)
+    {
+        Console.WriteLine(element);
+    }
+}
---
+++ OneDimensionArray/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*           0  1  2  3   4 5
+ array arr= {1,20,30,343,24,1}   ---> stack   heap
+                arr[1]
+                                        arr---->[1,20...]*/
+
+Console.WriteLine("Enter number in array:");
+int x = int.Parse(Console.ReadLine());
+
+//int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+int[] arr = new int[x];
+
+Console.WriteLine("before filling array");
+for (int i = 0; i < x; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+
+for (int i = 0; i < x; i++)
+{
+    Console.WriteLine($"Enter {i}th element:");
+    int k = int.Parse(Console.ReadLine());
+    arr[i] = k;
+}
+
+Console.WriteLine("after filling array");
+for (int i = 0; i < x; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+
+foreach (int el in arr)
+{
+    Console.Write($"{el} ");
+}
+
